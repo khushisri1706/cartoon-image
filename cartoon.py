20f7a9f8b5c1cbe6ee76844ac76fd56184e5c8e8
khushisri1706 <96279121+khushisri1706@users.noreply.github.com>
2022-11-30 14:56:04 +0530

    Add files via upload

---
+++ cartoon.py
@@ -0,0 +1,50 @@
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib
+import os
+
+
+def Image(img,frame_name):
+    output_folder = 'car.jpg'        # sample ->"Content\Examples"
+    cv2.imshow(frame_name,img)
+    cv2.imwrite(os.path.join(output_folder,frame_name+'.jpg'),img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+# TODO - To lighten the image and apply blur and detect sharp edges
+
+
+#Function to print Image
+try:
+    path='car.jpg'  #sample -> Content\\Download\\Pencil Sketch\\m0 (1).jpeg
+    img=cv2.imread(path)
+    height,width, channels = img.shape
+    #print(height,width)
+except AttributeError as AE:
+    print('----------Check Validity of Image path--------------')
+    exit() 
+except AssertionError as Asserr:
+    print('--------Check Path----')
+    exit()
+except Exception as e:
+    print(e)
+    print('-----------Please check your Image Path-------------')
+    exit()
+
+name = list(path.split('.'))[0] # Name of the image
+
+#Verify image 
+Image(img,'Original Image')
+
+col_img = cv2.bilateralFilter(img,5,255,255)
+Image(col_img,"After Applying Bilateral Filter")
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+Image(gray,'Grayscaled Image')
+gray = cv2.medianBlur(gray,3)
+Image(gray,'After applying Blur to Grayscale')
+edges = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,5,5)
+Image(edges,'Getting edges')
+cartoon = cv2.bitwise_and(col_img,col_img,mask=edges)
+Image(cartoon,'Cartoon')
+filename = name + '_cartoon.png'
+cv2.imwrite(filename,cartoon)
